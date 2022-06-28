7e5b60ae4c9fdf2db0968dd0ae738a56581ef2a3
Mads Kjeldgaard <mads@MacBook-Air.local>
2022-06-28 23:01:12 +0200

    add sc3-plugins

---
+++ sc3-plugins.sh
@@ -0,0 +1,33 @@
+INSTALL_LOCATION="$HOME/Library/Application Support/SuperCollider/Extensions"
+SCRIPTS_HOME="$(pwd)"
+SC_LOCATION="${SCRIPTS_HOME}/supercollider"
+PROJECT_DIR="sc3-plugins"
+OSX_ARCH=$1
+
+cd $SCRIPTS_HOME
+
+if [ -z "$OSX_ARCH" ]; then
+	echo "No target architecture chosen. Run the script again with either x86_64 or arm as the argument: ./script.sh x86_64" && exit 1;
+fi
+
+echo "Building $PROJECT_DIR for architecture $OSX_ARCH"
+cd $PROJECT_DIR
+
+git submodule update --init --recursive
+
+if [ -d build ]; then
+	rm -rf build
+else
+	mkdir build
+fi
+
+cd build
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DSC_PATH="$SC_LOCATION" -DCMAKE_INSTALL_PREFIX="$INSTALL_LOCATION" -DCMAKE_OSX_ARCHITECTURES="$OSX_ARCH" && \
+	cmake --build . --config Release && \
+	cmake --build . --config Release --target install && \
+	cd .. && \
+	rm -rf build && \
+	echo "Succesfully built and installed $PROJECT_DIR"
+
+cd $SCRIPTS_HOME
